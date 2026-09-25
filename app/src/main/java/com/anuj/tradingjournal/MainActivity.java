f16d509467acc3783140d58307971b9c4da93d80
mantanyadav00650266-ops <mantanyadav00650266@gmail.com>
2026-09-25 12:19:52 +0530

    Add files via upload

---
+++ app/src/main/java/com/anuj/tradingjournal/MainActivity.java
@@ -0,0 +1,47 @@
+package com.anuj.tradingjournal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(7, 16, 31));
+        getWindow().setNavigationBarColor(Color.rgb(5, 11, 20));
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(5, 11, 20));
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setSupportZoom(false);
+        s.setLoadWithOverviewMode(false);
+        s.setUseWideViewPort(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
